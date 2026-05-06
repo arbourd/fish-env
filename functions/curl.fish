fd17eb84139fc11f8a361e994f5778797ffd571e
Dylan Arbour <arbourd@users.noreply.github.com>
2026-05-06 00:26:20 -0400

    Add curlie alias

---
+++ functions/curl.fish
@@ -0,0 +1,3 @@
+function curl -w 'curlie' --description 'Alias for curlie'
+    curlie $argv
+end
--- functions/tf.fish
+++ functions/tf.fish
@@ -1,7 +1,3 @@
 function tf -w 'terraform' --description 'Alias for terraform'
     terraform $argv
 end
-
-function ot -w 'terraform' --description 'Alias for opentofu'
-    tofu $argv
-end
